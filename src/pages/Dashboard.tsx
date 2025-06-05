d789426113f675adce6e835c16ddfeb707ae9995
Shuashuaa <Shuashuaa@users.noreply.github.com>
2025-06-05 10:23:38 +0800

    first commit

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,19 @@
+export default function Home() {
+  return (
+    <div
+      style={{
+        flex: 1,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: '24px',
+        fontWeight: 'bold',
+        color: '#888',
+        flexDirection: 'column',
+        width: '100%',
+      }}
+    >
+      <h1 style={{ margin: 0 }}>Dashboard</h1>
+    </div>
+  );
+}
