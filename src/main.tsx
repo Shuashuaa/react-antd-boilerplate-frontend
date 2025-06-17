9f1f5cc6858866e93daa48d24810d5841632e2f7
Shuashuaa <Shuashuaa@users.noreply.github.com>
2025-06-17 10:37:34 +0800

    first commit

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from "react-router";
 import './index.css'
-import App from './App.tsx'
-import Login from './pages/auth/Login.tsx'
+// import App from './App.tsx'
+// import Login from './pages/auth/Login.tsx'
 import router from './routes'
 import '@ant-design/v5-patch-for-react-19'
 
