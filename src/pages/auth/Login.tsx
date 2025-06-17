376744dee3f55c249630a14c18b3440c7c99a19e
Shuashuaa <Shuashuaa@users.noreply.github.com>
2025-06-17 10:18:11 +0800

    first commit

---
+++ src/pages/auth/Login.tsx
@@ -0,0 +1,180 @@
+import { useState } from 'react';
+import { notification, type FormProps } from 'antd';
+import { Button, Checkbox, Form, Input } from 'antd';
+import { GoogleCircleFilled, FacebookFilled } from '@ant-design/icons';
+
+type FieldType = {
+    username?: string;
+    password?: string;
+    remember?: string;
+};
+
+const handleGoogleLogin = () => {
+    alert('Logging in with Google...');
+    // redirect to Google auth handler
+};
+
+const handleFacebookLogin = () => {
+    alert('Logging in with Facebook...');
+    // redirect to facebook auth handler
+};
+
+export default function Login(){
+
+    const [api, contextHolder] = notification.useNotification();
+
+    const openNotification = (title: string, pauseOnHover: boolean) => {
+        api.open({
+            message: title,
+            description: 'This is the content of the notification. This is the content of the notification. This is the content of the notification.',
+            showProgress: true,
+            // icon: <SmileOutlined style={{ color: '#108ee9' }} />,
+            pauseOnHover,
+        })
+    }
+
+    const onFinish: FormProps<FieldType>['onFinish'] = (values) => {
+        console.log('Success:', values);
+        enterLoading(0, 'Successfully Logged In!');
+    };
+
+    const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+        enterLoading(0, 'Failed to log in - Empty field');
+    };
+
+    const [loadings, setLoadings] = useState<boolean[]>([]);
+
+    const enterLoading = (index: number, title: string) => {
+        setLoadings((prevLoadings) => {
+            const newLoadings = [...prevLoadings];
+            newLoadings[index] = true;
+            return newLoadings;
+        })
+        setTimeout(() => {
+            setLoadings((prevLoadings) => {
+                const newLoadings = [...prevLoadings];
+                newLoadings[index] = false;
+                return newLoadings;
+            })
+
+            openNotification(title, true);
+        }, 3000)
+    }
+
+    return (
+        <>
+            <div
+                style={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100vh',
+                }}
+            >
+
+                {contextHolder}
+
+                {/* <img src="https://voidzero.dev/logo.svg" 
+                    alt="company icon - admin portal" 
+                    width={100}
+                /> */}
+
+                <h1>Login</h1>
+
+                <Form
+                    name="basic"
+                    layout='vertical'
+                    //   labelCol={{ span: 8 }}
+                    //   wrapperCol={{ span: 16 }}
+                    style={{ width: '400px', backgroundColor: 'white', display: 'flex', flexDirection: 'column', justifyContent: 'center' }}
+                    initialValues={{ remember: true }}
+                    onFinish={onFinish}
+                    onFinishFailed={onFinishFailed}
+                    autoComplete="on"
+                >
+                    <Form.Item<FieldType>
+                        style={{ marginBottom: '12px' }}
+                        label="Username"
+                        name="username"
+                        rules={[{ required: true, message: 'Please input your username!' }]}
+                    >
+                        <Input />
+                    </Form.Item>
+
+                    <Form.Item<FieldType>
+                        style={{ marginBottom: '12px' }}
+                        label="Password"
+                        name="password"
+                        rules={[{ required: true, message: 'Please input your password!' }]}
+                    >
+                        <Input.Password />
+                    </Form.Item>
+
+                    <Form.Item<FieldType> 
+                        style={{ marginBottom: '12px' }}
+                        name="remember" 
+                        valuePropName="checked" 
+                        label={null}
+                    >
+                        <Checkbox>Remember me</Checkbox>
+                    </Form.Item>
+
+                    <Form.Item 
+                        label={null}
+                        style={{ marginBottom: '12px' }}
+                    >
+                        <Button color="default" variant="solid" htmlType="submit" loading={loadings[0]} style={{ width: '100%' }}>
+                        Login
+                        </Button>
+                    </Form.Item>
+
+                    <Form.Item 
+                        label={null}
+                        style={{ marginBottom: '12px' }}
+                    >
+                        <div
+                            style={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            textAlign: 'center',
+                            gap: '10px',
+                            }}
+                        >
+                            <div style={{ flex: 1, height: '1px', backgroundColor: '#d9d9d9' }} />
+                                <span style={{ whiteSpace: 'nowrap', color: '#999' }}>or</span>
+                            <div style={{ flex: 1, height: '1px', backgroundColor: '#d9d9d9' }} />
+                        </div>
+                    </Form.Item>
+
+                    <Form.Item 
+                        label={null}
+                        style={{ marginBottom: '12px' }}
+                    >
+                        <div style={{ display: 'flex', justifyContent: 'center', gap: '10px' }}>
+                        <Button
+                            style={{ backgroundColor: '#db4437', color: 'white', width: '100%' }}
+                            icon={<GoogleCircleFilled />}
+                            onClick={handleGoogleLogin}
+                        >
+                            Google
+                        </Button>
+                        <Button
+                            style={{ backgroundColor: '#3b5998', color: 'white', width: '100%' }}
+                            icon={<FacebookFilled />}
+                            onClick={handleFacebookLogin}
+                        >
+                            Facebook
+                        </Button>
+                        </div>
+                    </Form.Item>
+                </Form>
+                <div style={{ marginTop: '12px' }}>
+                    <span>Not registered yet? </span>
+                    <a href="/register">Create an account</a>
+                </div>
+            </div>
+        </>
+    )
+}
