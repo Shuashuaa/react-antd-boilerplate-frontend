4636f21d2c7d210d1a15393def0c9256a81a0450
Shuashuaa <Shuashuaa@users.noreply.github.com>
2025-06-18 15:29:39 +0800

    Routing with Protected Component Wrapper

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,27 +1,34 @@
 import { createBrowserRouter } from "react-router";
 import Layout from "./layout/Layout";
-// import App from "./pages/App";
 import Home from "./pages/Home";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import Dashboard from "./pages/Dashboard";
 import NotFound from "./pages/NotFound";
+import ProtectedRoute from "./utils/ProtectedRoutes";
+import AuthRedirectRoute from "./utils/AuthRedirectRoute";
 
 const router = createBrowserRouter([
     {
-        path: "/login",
-        Component: Login,
-    },
-    {
-        path: "/register",
-        Component: Register,
+        path: "/",
+        Component: ProtectedRoute,
+        children: [
+            {
+                path: "/",
+                Component: Layout,
+                children: [
+                    { index: true, Component: Home },
+                    { path: "dashboard", Component: Dashboard }
+                ],
+            }
+        ],
     },
     {
         path: "/",
-        Component: Layout,
+        Component: AuthRedirectRoute,
         children: [
-            { index: true, Component: Home },
-            { path: "dashboard", Component: Dashboard }
+            { path: "login", Component: Login },
+            { path: "register", Component: Register },
         ],
     },
     {
